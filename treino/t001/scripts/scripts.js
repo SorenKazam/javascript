5ac56e8cd1ad2aea323545fb81820509d7aaa37c
Tiago Guerreiro <46906203+SorenKazam@users.noreply.github.com>
2023-12-18 00:21:34 +0000

    numeros sao adicionados

--- treino/t001/scripts/scripts.js
+++ treino/t001/scripts/scripts.js
@@ -12,13 +12,20 @@ var btnNumThree = document.querySelector('td#btn-num-three');
 var btnNumFour = document.querySelector('td#btn-num-four')
 var btnNumFive = document.querySelector('td#btn-num-five')
 var btnNumSix = document.querySelector('td#btn-num-six')
+var btnNumSeven = document.querySelector('td#btn-num-seven')
+var btnNumEight = document.querySelector('td#btn-num-eight')
+var btnNumNine = document.querySelector('td#btn-num-nine')
 //action buttons
 var btnActionAc = document.querySelector('td#btn-action-ac')
 var btnActionEnter = document.querySelector('td#btn-action-enter')
+var btnActionDot = document.querySelector('td#btn-action-dot')
 // operator buttons
 var btnOpSoma = document.querySelector('td#btn-op-soma')
+var btnOpSubt = document.querySelector('td#btn-op-subt')
+var btnOpDivi = document.querySelector('td#btn-op-divi')
+var btnOpMult = document.querySelector('td#btn-op-mult')
 
-// adding button numbers to the buttons
+// adding numbers to the buttons
 btnNumZero.innerHTML = '0';
 btnNumOne.innerHTML = '1';
 btnNumTwo.innerHTML = '2';
@@ -26,30 +33,44 @@ btnNumThree.innerHTML = '3';
 btnNumFour.innerHTML = '4';
 btnNumFive.innerHTML = '5';
 btnNumSix.innerHTML = '6';
+btnNumSeven.innerHTML = '7';
+btnNumEight.innerHTML = '8';
+btnNumNine.innerHTML = '9';
+// adding operators to the buttons
+btnOpSoma.innerHTML = '+'
+btnOpSubt.innerHTML = '-'
+btnOpDivi.innerHTML = '/'
+btnOpMult.innerHTML = '*'
+// adding action buttons to the buttons
+btnActionAc.innerHTML = 'AC'
+btnActionEnter.innerHTML = '='
+btnActionDot.innerHTML = '.'
 
 // calc display default cursor
 if (currentNumber.length === 0){
     calcDisplay.textContent = '0'
 }
 
-btnActionAc.addEventListener('click', clear)
+// Agrupando todos os butoes numericos
+var numericButtons = document.querySelectorAll('.numeric-button')
 
 // add digits
-btnNumZero.addEventListener('click', function() {
-    if (currentNumber.length !== 0){
-        addDigit('0')
-    } else {
-        console.error('Zerro')
-    }
+numericButtons.forEach(function(button){
+    button.addEventListener('click', function(){
+        if (button.textContent === '0'){
+            if (currentNumber.length !== 0){
+                addDigit(button.textContent)
+            } else {
+                console.error('Zerro')
+            }
+        } else {
+            addDigit(button.textContent)
+        }
+    })
 })
 
-btnNumOne.addEventListener('click', function() {
-    addDigit('1')
-})
-
-btnNumTwo.addEventListener('click', function(){
-    addDigit('2')
-})
+// butao all clear
+btnActionAc.addEventListener('click', clear)
 
 // add operators
 btnOpSoma.addEventListener('click', function(){
@@ -69,8 +90,6 @@ function clear() {
 function addDigit(digit){
     currentNumber += digit
     updateDisplay()
-    console.log('displayNumbers: ' + displayNumbers)
-    console.log('currentNumbers:' + currentNumber)
 }
 
 function addOperator(operator){
@@ -84,49 +103,7 @@ function addOperator(operator){
 
 function updateDisplay() {
     if(currentNumber === '89572023121'){
-        console.log('Welcome DocMods')
+        window.alert('Welcome DocMods')
     }
     calcDisplay.textContent = displayNumbers.join(' ') + ' ' + currentNumber
 }
-
-
-/* 
-<section class="calc">
-        <table>
-            <thead>
-                <tr>
-                    <td colspan="4" id="display">342423789416237841623948172649823764982376149823674192387425452345234523452345236.359</td>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td id="btn-action-ac">AC</td>
-                    <td id="btn-op-divi">/</td>
-                    <td id="btn-op-mult">*</td>
-                    <td id="btn-op-subt">-</td>
-                </tr>
-                <tr>
-                    <td id="btn-num-seven">7</td>
-                    <td id="btn-num-eight">8</td>
-                    <td id="btn-num-nine">9</td>
-                    <td rowspan="2" id="btn-op-soma">+</td>
-                </tr>
-                <tr>
-                    <td id="btn-num-four">4</td>
-                    <td id="btn-num-five">5</td>
-                    <td id="btn-num-six">6</td>
-                </tr>
-                <tr>
-                    <td id="btn-num-one">1</td>
-                    <td id="btn-num-two">2</td>
-                    <td id="btn-num-three">3</td>
-                    <td rowspan="2" id="btn-">Enter</td>
-                </tr>
-                <tr>
-                    <td colspan="2" id="btn-num-zero">0</td>
-                    <td id="btn-action-dot">.</td>
-                </tr>
-            </tbody>
-        </table>
-    </section>
-*/
