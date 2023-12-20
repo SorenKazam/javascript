8415dffb9078f6e363f9e470d274eb5d9c423e62
Tiago Guerreiro <46906203+SorenKazam@users.noreply.github.com>
2023-12-20 14:52:40 +0000

    aula11 criada

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro e ${vel}Km/h.`)
+
+if (vel > 60){ // condicao simples
+    console.log('Voce ultrapassou a velocidade permitida! Multado.')
+}
+
+console.log('Dirija sempre usando cinto de seguranca!')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Estados Unidos';
+console.log(`Vivendo em: ${pais}`)
+if(pais == 'Portugal'){
+    console.log('Portugues!')
+} else {
+    console.log('Estrangeiro')
+}
